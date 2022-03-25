5092e8914c8e677c58b73fe8fa7cc58cd25bd633
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-25 12:34:12 +0000

    Brew formula update for proton version v2.1.0

--- Formula/proton.rb
+++ Formula/proton.rb
@@ -5,23 +5,31 @@
 class Proton < Formula
   desc "cli protobuf to json converter"
   homepage "https://github.com/beatlabs/proton"
-  version "2.0.2"
+  version "2.1.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/beatlabs/proton/releases/download/v2.0.2/proton_Darwin_x86_64.tar.gz"
-      sha256 "52ea01f462168327669fdb0b10f59953086c2ab1762bd872913adc8c2af54e6c"
+    url "https://github.com/beatlabs/proton/releases/download/v2.1.0/proton_Darwin_x86_64.tar.gz"
+    sha256 "dfcae8d7e2d67c7e0c5455a274dc7ffb3dcf5f07f3c66c518247cf3a1bdac586"
 
-      def install
-        bin.install "proton"
+    def install
+      bin.install "proton"
+    end
+
+    if Hardware::CPU.arm?
+      def caveats
+        <<~EOS
+          The darwin_arm64 architecture is not supported for the Proton
+          formula at this time. The darwin_amd64 binary may work in compatibility
+          mode, but it might not be fully supported.
+        EOS
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/beatlabs/proton/releases/download/v2.0.2/proton_Linux_x86_64.tar.gz"
-      sha256 "9644df017d70fa0e40755abc9b43e2dd56c0fa7611a142e7b3a1f8ffee9772b2"
+      url "https://github.com/beatlabs/proton/releases/download/v2.1.0/proton_Linux_x86_64.tar.gz"
+      sha256 "35e3fdf299aacbf5baee62d755a8a4b9ff52c9f7f3380d3b7e92bf52a77b5dcf"
 
       def install
         bin.install "proton"
